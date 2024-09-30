aefb24c68b2baac1938b656c635b078d028e3b99
Levi Planelles <leviplanellesgranata@gmail.com>
2024-09-30 20:40:44 +0200

    pues otra modificacion

--- src/main/java/com/leviplanelles/tema03/Ejercicio1.java
+++ src/main/java/com/leviplanelles/tema03/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.leviplanelles.tema03;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Esto es un ejercicio 1 pruebaa");
+        System.out.println("Segunda pruebaaa");
     }
 }
