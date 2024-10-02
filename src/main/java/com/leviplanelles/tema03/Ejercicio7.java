c7c4b1e4b2d0c706a137ee8763076f70db222d8b
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-02 15:54:53 +0200

    Act6 terminada

---
+++ src/main/java/com/leviplanelles/tema03/Ejercicio7.java
@@ -0,0 +1,11 @@
+package com.leviplanelles.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
