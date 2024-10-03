20c7c30439928d2a442da94bf401911132df7768
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-04 00:28:10 +0200

    si

--- src/main/java/com/leviplanelles/tema03/bucles/Ejercicio3.java
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio3.java
@@ -7,12 +7,17 @@ public class Ejercicio3 {
         Scanner leer = new Scanner(System.in);
         int num1;
         int num2;
-
+        int resultado = 0;
         System.out.println("Dime un número: ");
         num1 = Integer.parseInt(leer.nextLine());
 
         System.out.println("Dime otro número: ");
         num2 = Integer.parseInt(leer.nextLine());
 
+        for (int i = 0; i < num2; i++) {
+            resultado += num1;
+        }
+
+        System.out.println("El resultado de "+ num1 + " por "+ num2+ " es " + resultado);
     }
 }
