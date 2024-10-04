8c3f496e82d61867ff542d95e24ce9e7f66c70bb
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-04 14:42:10 +0200

    ejercicio 4 y 5 hechos

---
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio4.java
@@ -0,0 +1,17 @@
+package com.leviplanelles.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int contador = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+                contador++;
+            }
+        }
+        System.out.println("Del 1 al 100 hay " + contador + " números impares.");
+    }
+}
---
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio5.java
@@ -0,0 +1,38 @@
+package com.leviplanelles.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int sumaPar = 0;
+        int sumaImpar = 0;
+        int contador = 0;
+        int sumaMultiplos = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("El número " + i + " Es par.");
+                sumaPar += i;
+                if (i % 5 == 0) {
+                    contador++;
+                    sumaMultiplos += i;
+
+                }
+            } else {
+                System.out.println("El número " + i + " Es impar.");
+                sumaImpar += i;
+                if (i % 5 == 0) {
+                    contador++;
+                    sumaMultiplos += i;
+
+                }
+            }
+
+        }
+        System.out.println("La suma de todos los pares es " + sumaPar);
+        System.out.println("La suma de todos los impares es " + sumaImpar);
+        System.out.println("En los números del 1 al 100 hay " + contador + " multiplos de 5 y todos ellos juntos suman " + sumaMultiplos);
+
+    }
+}
