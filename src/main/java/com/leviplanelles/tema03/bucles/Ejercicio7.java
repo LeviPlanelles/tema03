351e2a75b6b775751cd9e6aef241338b3464e126
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-08 16:00:14 +0200

    potencia negativa ej7

--- src/main/java/com/leviplanelles/tema03/bucles/Ejercicio7.java
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio7.java
@@ -15,9 +15,14 @@ public class Ejercicio7 {
         System.out.print("Dime una potencia para el número anterior: ");
         potencia = Integer.parseInt(leer.nextLine());
         leer.close();
+        int signo = potencia < 0 ? -1 : 1;
+        potencia *= signo;
         for (int i = 1; i <= potencia; i++) {
             resultado *= numReal;
         }
+        potencia *= signo;
+        resultado = signo < 0 ? 1 / resultado : resultado;
+
         System.out.println("El número " + numReal + " elevado a " + potencia + " es igual a " + resultado);
 
     }
