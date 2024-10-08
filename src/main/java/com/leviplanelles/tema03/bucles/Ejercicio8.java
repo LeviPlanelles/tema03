03de36bab0d2e085f49c688fc57a09ff8534d8a6
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-08 12:36:59 +0200

    act 7,8,9

--- src/main/java/com/leviplanelles/tema03/bucles/Ejercicio8.java
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio8.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+        int num;
         int resultadoFactorial = 1;
+        System.out.print("Dime un numero y te hago su factorial: ");
+        num = Integer.parseInt(leer.nextLine());
 
-        for (int i = 4; i > 0; i--) {
+        for (int i = num; i > 0; i--) {
             resultadoFactorial *= i;
         }
-        System.out.println(resultadoFactorial);
+        System.out.println(num + "! es igual a " + resultadoFactorial);
 
     }
 }
