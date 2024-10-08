05748a8111213bbf381be55d3bbb622e68b3a841
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-08 19:21:02 +0200

    ejercicio12

--- src/main/java/com/leviplanelles/tema03/bucles/Ejercicio12.java
+++ src/main/java/com/leviplanelles/tema03/bucles/Ejercicio12.java
@@ -1,6 +1,6 @@
 package com.leviplanelles.tema03.bucles;
 
-import java.sql.SQLOutput;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,16 +8,50 @@ public class Ejercicio12 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         Random random = new Random();
-        int intentos;
+        int intentos, guess;
+        int numRandom = random.nextInt(1,101);
+        System.out.println(numRandom);
 
         System.out.println("****************");
         System.out.println("GUESS THE NUMBER");
         System.out.println("****************\n");
 
-        System.out.println("Generando un numero aleatorio...\n");
-        int numRandom = random.nextInt(1,101);
-        System.out.print("Indica el número de intentos que vas a tener para adivinar el número: ");
-        intentos = Integer.parseInt(leer.nextLine());
+        System.out.println("Generando un numero aleatorio del 1 al 100...\n");
+        do {
+            System.out.print("Indica el número de intentos que vas a tener para adivinar el número: ");
+            intentos = Integer.parseInt(leer.nextLine());
+            boolean error = intentos < 1;
+            String mensaje = error ? "Vuelve a intentarlo" : "Adelante!\n";
+            System.out.println(mensaje);
+        }while (intentos < 1);
+
+
+        for (int i = 1; i <= intentos;) {
+            System.out.print("Adivina el numerito mágico: ");
+            guess = Integer.parseInt(leer.nextLine());
+            if (guess != numRandom) {
+                System.err.println("Número incorrecto, intentalo otra vez");
+                intentos--;
+                System.out.println("Te quedan " + intentos + " intentos.\n");
+                if (guess < numRandom) {
+                    System.out.println("TIP: El número mágico es más grande que " + guess + "\n");
+                }else {
+                    System.out.println("TIP: El número mágico es mas pequeño que " + guess + "\n");
+                }
+            }else {
+                System.out.println("¡¡FELICIDADES HAS ACERTADO EL NÚMERO!!");
+                System.out.println("**************************************");
+                break;
+            }
+
+
+        }
+        if (intentos == 0) {
+            System.out.println("Se te han acabado los intentos, has perdido!");
+
+        }
+
+
 
     }
 }
