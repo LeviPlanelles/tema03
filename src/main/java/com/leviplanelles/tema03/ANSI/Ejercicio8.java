f4642189d6e6cc9b24af6e3de016ae8adfa370d7
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-16 17:18:27 +0200

    ejercicio8

---
+++ src/main/java/com/leviplanelles/tema03/ANSI/Ejercicio8.java
@@ -0,0 +1,29 @@
+package com.leviplanelles.tema03.ANSI;
+
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        final String ESC = "\u001B[";
+        final String RESET = ESC + "0m";
+        int color = 29;
+        int bg = 40;
+        int intensidad = 0;
+        final int COLUMNAS = 8;
+        final int FILAS = 16;
+
+        for (int j = 1; j <= FILAS; j++) {
+            color = color == 37 ? 30 : ++color;
+            for (int i = 1; i <= COLUMNAS; i++) {
+                System.out.printf(ESC + intensidad + ";" + color + ";" + bg + "m %d;%d;%d "+RESET,intensidad,color,bg);
+                bg = bg == 47 ? 40 : ++bg;
+            }
+            if (j >= 8) {
+                intensidad = 1;
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
