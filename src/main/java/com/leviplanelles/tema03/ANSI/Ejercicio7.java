bdd60612639a0bdd439757d70a9e4d13fd9a6636
Levi Planelles <leviplanellesgranata@gmail.com>
2024-10-16 16:16:28 +0200

    ejercicio7

--- src/main/java/com/leviplanelles/tema03/ANSI/Ejercicio7.java
+++ src/main/java/com/leviplanelles/tema03/ANSI/Ejercicio7.java
@@ -11,15 +11,15 @@ public class Ejercicio7 {
 
         }*/
         String cadena = "";
-        String cadena1 = "";
         for (int i = 1; i <= 9; i++) {
             cadena += i;
-            System.out.printf("%9s\n",cadena);
-            for (int j = 0; j <= 1; j++) {
-                cadena1 = j + cadena1;
+            System.out.printf("%9s",cadena);
+            for (int j = (cadena.length()-1); j >= 0; j--) {
+                System.out.print(cadena.charAt(j));
             }
-
+            System.out.println();
         }
+
 
     }
 }
